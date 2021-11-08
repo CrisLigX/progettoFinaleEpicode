5f1ccb9fd60f3fcf88f56ec09cb6fea073617914
cristian.a.liguori <calworks93@gmail.com>
2021-11-08 12:39:01 +0100

    Progetto finale di Epicode - Test di VSCode

--- ngCRMEpicoders/src/app/app.module.ts
+++ ngCRMEpicoders/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyHeaderComponent } from './my-header/my-header.component';
+import { MyFooterComponent } from './my-footer/my-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHeaderComponent,
+    MyFooterComponent
   ],
   imports: [
     BrowserModule
